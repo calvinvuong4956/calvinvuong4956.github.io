2e34ac2ac2f8d1ea65c7a9b15617c3cd084c257d
Calvin V. <caltelpa1@gmail.com>
2025-09-21 03:16:18 +1000

    assignment 2 W.I.P

---
+++ assignment2/script.js
@@ -0,0 +1,156 @@
+// songs
+const songs = [
+  {
+    src: "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/erokia_ambient-wave-56-msfxp7-78.mp3",
+    cover:
+      "https://images.stockcake.com/public/0/d/f/0df9e740-854f-422d-b01a-37baf403883a_large/silhouette-meditation-outdoors-stockcake.jpg",
+    alt: "meditation album cover",
+  },
+  {
+    src: "jazz.mp3",
+    cover:
+      "https://images.stockcake.com/public/6/d/2/6d2f5c4c-6c93-4822-a589-40b1989563ce_large/ambient-jazz-night-stockcake.jpg",
+    alt: "jaxx album cover",
+  },
+  {
+    src: "lofi.mp3",
+    cover: "https://f4.bcbits.com/img/a1435058381_16.jpg",
+    alt: "lofi album cover",
+  },
+];
+
+let currentSongIndex = 0;
+
+// access to needed elements
+const audioM = document.querySelector("#meditation");
+console.log(audioM);
+const musicCover = document.querySelector(".music-cover");
+console.log(musicCover);
+const musicCoverContainer = document.querySelector(".music-cover-container");
+console.log(musicCoverContainer);
+const progressBar = document.querySelector("#progress-bar-fill");
+console.log(progressBar);
+
+// click event-listener for elements
+musicCover.addEventListener("click", togglePlayPause);
+
+// progress-bar update (replace "video" with "audio" since I'm working with audio files)
+audioM.addEventListener("timeupdate", updateProgressBar);
+
+function togglePlayPause() {
+  if (audioM.paused || audioM.ended) {
+    audioM.play();
+    hidePauseIcon();
+  } else {
+    audioM.pause();
+    showPauseIcon();
+  }
+}
+function updateProgressBar() {
+  if (audioM.duration) {
+    const value = (audioM.currentTime / audioM.duration) * 100;
+    progressBar.style.width = value + "%";
+  }
+}
+
+// Add other functionalities here
+// pause overlay
+const pauseOverlay = document.createElement("div");
+console.log(pauseOverlay);
+
+pauseOverlay.classList.add("pause-overlay");
+pauseOverlay.innerHTML = `<img src="https://img.icons8.com/ios-glyphs/64/pause--v1.png" alt="PAUSE" class="pause-icon"> 
+`;
+pauseOverlay.style.display = "flex";
+
+// pause overlay appear over music-cover-container
+musicCoverContainer.style.position = "relative";
+musicCoverContainer.appendChild(pauseOverlay);
+
+// function for pause icon appearing/disappearing
+function showPauseIcon() {
+  pauseOverlay.style.display = "flex";
+}
+
+function hidePauseIcon() {
+  pauseOverlay.style.display = "none";
+}
+
+// Mute/Unmute
+const muteUnmuteButton = document.querySelector("#mute-unmute-button");
+console.log(muteUnmuteButton);
+
+const muteUnmuteImg = document.querySelector("#mute-unmute-img");
+console.log(muteUnmuteImg);
+
+// this code makes it so the volume and its icon is UNMUTED upon initial startup of the website
+audioM.muted = false;
+muteUnmuteImg.src = "https://img.icons8.com/ios-glyphs/30/high-volume--v2.png";
+
+// - addEventListener to code "CLICK" to button
+muteUnmuteButton.addEventListener("click", toggleAudio);
+
+// - write callback function required for MUTE/UNMUTE function
+// - change mute/unmute icon
+function toggleAudio() {
+  if (audioM.muted) {
+    audioM.muted = false;
+    muteUnmuteImg.src =
+      "https://img.icons8.com/ios-glyphs/30/high-volume--v2.png";
+  } else {
+    audioM.muted = true;
+    muteUnmuteImg.src = "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
+  }
+}
+
+// Song Music Cover Change
+function loadSong(index) {
+  const song = songs[index];
+  audioM.src = song.src;
+  musicCover.src = song.cover;
+  musicCover.alt = song.alt;
+
+  progressBar.style.width = "0%";
+  showPauseIcon();
+}
+
+// Next-Previous Song
+const nextButton = document.querySelector("#next");
+console.log(nextButton);
+const previousButton = document.querySelector("#previous");
+console.log(previousButton);
+
+nextButton.addEventListener("click", nextSong);
+previousButton.addEventListener("click", previousSong);
+
+function nextSong() {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  loadSong(currentSongIndex);
+}
+
+function previousSong() {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  loadSong(currentSongIndex);
+}
+
+// Interactive List (header)
+const jazzNav = document.querySelector("#jazz-nav");
+const meditationNav = document.querySelector("#meditation-nav");
+const lofiNav = document.querySelector("#lofi-nav");
+
+function playListSong(songIndex) {
+  currentSongIndex = songIndex;
+  loadSong(currentSongIndex);
+}
+
+jazzNav.addEventListener("click", (e) => {
+  playListSong(1);
+});
+
+meditationNav.addEventListener("click", (e) => {
+  playListSong(0);
+});
+
+lofiNav.addEventListener("click", (e) => {
+  playListSong(2);
+});
