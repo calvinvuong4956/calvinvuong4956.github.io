663a41485e2c0588df5397dd18660bc68c959da4
Calvin V. <caltelpa1@gmail.com>
2025-09-24 18:35:40 +1000

    week9
    FIRST CSS TRANSITIONS AND ANIMATIONS

---
+++ week9/script.js
@@ -0,0 +1,18 @@
+const clickButton = document.querySelector("#click-button");
+console.log(clickButton);
+const myDuck = document.querySelector("#my-duck");
+console.log(myDuck);
+
+let counter = 0;
+
+clickButton.addEventListener("click", moveDuck);
+
+function moveDuck() {
+  if (counter === 0) {
+    myDuck.style.translate = "20px -40px";
+    counter = 1;
+  } else {
+    myDuck.style.translate = "0px 0px";
+    counter = 0;
+  }
+}
