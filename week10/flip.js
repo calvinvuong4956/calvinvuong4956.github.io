d3603593490f05d4826efe04a76ad483a4575290
Calvin V. <caltelpa1@gmail.com>
2025-10-01 19:06:07 +1000

    week10
    Examples

---
+++ week10/flip.js
@@ -0,0 +1,21 @@
+const card = document.querySelector(".card");
+console.log(card);
+
+// HOVER CODE
+// -----------------------------------------------------------
+// card.addEventListener("mouseenter", addFlip);
+// card.addEventListener("mouseleave", removeFlip);
+
+// function addFlip() {
+//   card.classList.add("flip");
+// }
+// function removeFlip() {
+//   card.classList.remove("flip");
+// }
+// -----------------------------------------------------------
+
+card.addEventListener("click", toggleFlip);
+
+function toggleFlip() {
+  card.classList.toggle("flip");
+}
